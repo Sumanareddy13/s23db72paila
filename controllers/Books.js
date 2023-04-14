51cf9c8cab216e0bb98babec7ffd6b7288b4caae
Sumanasri Reddy <pailasumanasri@gmail.com>
2023-04-14 13:12:51 -0500

    modified books in controllers

--- controllers/Books.js
+++ controllers/Books.js
@@ -49,7 +49,7 @@ exports.Books_view_all_Page = async function(req, res) {
    };
 
 
-   // Handle Costume create on POST.
+   // Handle Books create on POST.
 exports.Books_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Books();
@@ -68,4 +68,38 @@ exports.Books_create_post = async function(req, res) {
     res.status(500);
     res.send(`{"error": ${err}}`);
     }
+};
+
+// for a specific Books.
+exports.Books_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Books.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
+
+    // Handle Books update form on PUT.
+exports.Books_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await Books.findById( req.params.id)
+// Do updates of properties
+if(req.body.book_name)
+toUpdate.book_name = req.body.book_name;
+if(req.body.book_genre) toUpdate.book_genre = req.body.book_genre;
+if(req.body.book_cost) toUpdate.book_cost = req.body.book_cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
 };
